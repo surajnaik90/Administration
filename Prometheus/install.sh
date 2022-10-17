72c6cbac9b48e44917081f62b0e1aa887d0840a6
surajnaik90 <107405512+surajnaik90@users.noreply.github.com>
2022-10-17 15:40:21 +0530

    Update install.sh

--- Prometheus/install.sh
+++ Prometheus/install.sh
@@ -13,3 +13,6 @@ mv prometheus-2.39.1.linux-amd64 prometheus
 
 cd prometheus
 
+#Create a user to run prometheus: Do no create a home directory, deny shell access to the user account
+sudo useradd --no-create-home --shell /bin/false prometheus
+
