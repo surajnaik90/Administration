8941d7918328331fbdd0121c5e7f193c1fb4569f
surajnaik90 <107405512+surajnaik90@users.noreply.github.com>
2022-10-17 15:42:36 +0530

    Update install.sh

--- Prometheus/install.sh
+++ Prometheus/install.sh
@@ -16,3 +16,6 @@ cd prometheus
 #Create a user to run prometheus: Do no create a home directory, deny shell access to the user account
 sudo useradd --no-create-home --shell /bin/false prometheus
 
+#Create config files directory & application state directory
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
