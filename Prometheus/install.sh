72af3525a8ac2847cba336f08a55875353d9c4f8
surajnaik90 <107405512+surajnaik90@users.noreply.github.com>
2022-10-17 15:25:19 +0530

    Update install.sh

--- Prometheus/install.sh
+++ Prometheus/install.sh
@@ -1,3 +1,15 @@
+#On Ubuntu 20.04.5 LTS
+
 #Update repositories
-sudo yum update -y
+sudo apt update
+
+mkdir prometheus
+
+curl -# -O https://github.com/prometheus/prometheus/releases/download/v2.39.1/prometheus-2.39.1.linux-amd64.tar.gz
+
+tar -xv prometheus-2.39.1.linux-amd64.tar.gz
+
+mv prometheus-2.39.1.linux-amd64 prometheus
+
+cd prometheus
 
